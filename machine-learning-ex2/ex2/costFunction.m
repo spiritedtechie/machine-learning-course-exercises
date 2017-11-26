7ae2ec78b688e362743d417d672674c0e59479f8
Vijay Patel <veejspeej@gmail.com>
2017-11-26 19:19:59 +0000

    implemeted cost fuction

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,11 +21,11 @@ grad = zeros(size(theta));
 %
 
 
+hypothesis = sigmoid(X * theta);
 
+J = 1 / m * sum((-y .* log(hypothesis)) - ((1 - y) .* log(1 - hypothesis)));
 
-
-
-
+grad = 1 / m * sum((hypothesis - y) .* X);
 
 % =============================================================
 
