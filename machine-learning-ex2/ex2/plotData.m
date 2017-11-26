d283440f3837469233ed7660fcae438914b9502b
Vijay Patel <veejspeej@gmail.com>
2017-11-26 17:50:12 +0000

    sigmoid

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -19,10 +19,7 @@ neg = find(y==0);
 plot(X(pos, 1), X(pos,2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
 plot(X(neg, 1), X(neg,2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7)
 
-
 % =========================================================================
-
-
 
 hold off;
 
